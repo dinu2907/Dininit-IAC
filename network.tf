9bac37ad3224b9d2b344aecf517ce979e4f22b72
dinu2907 <82222946+dinu2907@users.noreply.github.com>
2025-03-30 14:20:17 +0530

    Create network.tf

---
+++ network.tf
@@ -0,0 +1,41 @@
+# 🔹 Create Virtual Network & Subnet
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myAKSVNet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "aks_subnet" {
+  name                 = "myAKSSubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# 🔹 Create Network Security Group & Allow All Traffic
+resource "azurerm_network_security_group" "nsg" {
+  name                = "myAKSNSG"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_network_security_rule" "allow_all" {
+  name                        = "AllowAllTraffic"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "0.0.0.0/0"
+  destination_address_prefix  = "0.0.0.0/0"
+  network_security_group_name = azurerm_network_security_group.nsg.name
+  resource_group_name         = azurerm_resource_group.rg.name
+}
+
+# 🔹 Associate NSG with Subnet
+resource "azurerm_subnet_network_security_group_association" "aks_nsg_assoc" {
+  subnet_id                 = azurerm_subnet.aks_subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
