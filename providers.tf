d5edcfbc5af2863ea5117223af18f587b99d3d15
dinu2907 <82222946+dinu2907@users.noreply.github.com>
2025-04-12 14:50:52 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -11,9 +11,9 @@ terraform {
 provider "azurerm" {
   features {}
 
-    # Use the Service Principal for authentication
-  client_id       = "04a73b31-9ae2-45ed-ad40-2fc91b5145f4"
-  client_secret   = "J4O8Q~mKTp4XlKJVzDI51gGgkprm4K.ZFI_1WalT"
-  tenant_id       = "e76f4fb2-c488-42f7-a7b4-18d2fa2ea405"
-  subscription_id = "3502cdf1-2daf-4167-a8dc-5e4a39bbe518"
+  # Use environment variables for Service Principal authentication
+  client_id       = env("ARM_CLIENT_ID")
+  client_secret   = env("ARM_CLIENT_SECRET")
+  tenant_id       = env("ARM_TENANT_ID")
+  subscription_id = env("ARM_SUBSCRIPTION_ID")
 }
