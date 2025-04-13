c9806fd3838bfddfa9fc44b772c5312086770b6d
dinu2907 <82222946+dinu2907@users.noreply.github.com>
2025-04-13 12:25:07 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,18 +2,18 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.80"  # Choose the older version (e.g., 2.x series)
+      version = "~> 3.80"  # Adjust this version as needed
     }
   }
+
   required_version = ">= 0.12"
 }
 
 provider "azurerm" {
-  features = {}
+  features {}
 
   client_id       = var.APP_ID
   client_secret   = var.CLIENT_SECRET
   subscription_id = var.SUBSCRIPTION_ID
   tenant_id       = var.TENANT_ID
 }
-
