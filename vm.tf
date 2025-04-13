fc1336526cdaaf2627294e6098df779418ed5c6c
dinu2907 <82222946+dinu2907@users.noreply.github.com>
2025-04-13 18:42:22 +0530

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -1,17 +1,3 @@
-# 🔹 Network Interface for VM
-resource "azurerm_network_interface" "vm_nic" {
-  name                = "testVMNIC"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.aks_subnet.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-# 🔹 Linux Virtual Machine (with password authentication)
 resource "azurerm_linux_virtual_machine" "test_vm" {
   name                = "testVM"
   location            = azurerm_resource_group.rg.location
@@ -32,15 +18,10 @@ resource "azurerm_linux_virtual_machine" "test_vm" {
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18_04-lts"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
     version   = "latest"
   }
 
   disable_password_authentication = false
-}
-
-# Optional output
-output "test_vm_private_ip" {
-  value = azurerm_network_interface.vm_nic.private_ip_address
 }
